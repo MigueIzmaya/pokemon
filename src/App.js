34dd29961f50618eb15a5202f536cefceec01d63
MigueIzmaya <m.izmaya@hotmail.com>
2022-03-31 23:33:25 -0600

    add landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListWidget from './components/ItemListWidget';
 
 const App = () => {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <ItemListWidget></ItemListWidget>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,37 @@
 import './NavBar.css';
+import './CartWidget.js';
+import CartWidget from './CartWidget.js';
   
   const NavBar = () => {
     return (
       <div className="NavBar"> 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Pokémon</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Pokémon</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
     
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Inicio</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="#">Inicio</a>
                         </li>
         
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tipos de cartas</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Tipos de cartas</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tipos de Pokémon</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Tipos de Pokémon</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Juegos</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Juegos</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Acerca de nosotros</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Acerca de nosotros</a>
                         </li>
                     </ul>
+                    <CartWidget></CartWidget>
                 </div>
             </div>
         </nav>
