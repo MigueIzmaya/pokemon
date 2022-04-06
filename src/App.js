8330a266d60f77f2e66b7dc25822f76585f1a737
MigueIzmaya <m.izmaya@hotmail.com>
2022-04-05 21:35:44 -0500

    Added item count

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListWidget from './components/ItemListWidget';
+import ItemCount from './components/ItemCount';
 
 const App = () => {
   return (
     <div className="App">
       <NavBar></NavBar>
       <ItemListWidget></ItemListWidget>
+      <ItemCount stock="5" initial="1"></ItemCount>
     </div>
   );
 }
