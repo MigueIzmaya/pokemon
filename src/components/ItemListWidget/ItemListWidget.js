6aee8b9402a0238497621105763005d61b7013ab
MigueIzmaya <m.izmaya@hotmail.com>
2022-04-13 00:58:05 -0500

    Add update setTimeout

--- src/components/ItemListWidget/ItemListWidget.js
+++ src/components/ItemListWidget/ItemListWidget.js
@@ -1,46 +1,60 @@
 import Item from '../Item/Item.js';
+import { useEffect, useState } from 'react';
+
 
 const ItemListWidget = () => {
 
-    const data = [
-        {
-            id: 1,
-            title: "Ultra Ball",
-            picture: "",
-            price: "$50"
+    const [information, setInformation] = useState([]);
 
-        },
-        {
-            id: 1,
-            title: "Double Turbo Energy",
-            picture: "",
-            price: "$75"
-        }
-    ]
+    useEffect(() => {
 
-    let is_stock = true;
+        let is_stock = true;
 
-    let products = (timeout, task) => {
-        return new Promise((resolve, reject) => {
-            if (is_stock) {
-                setTimeout(() => {
-                    resolve(task)
-                }, timeout);
-            } else {
-                reject("Error")
+        const data = [
+            {
+                id: 1,
+                title: "Ultra Ball",
+                picture: "",
+                price: "$50"
+    
+            },
+            {
+                id: 1,
+                title: "Double Turbo Energy",
+                picture: "",
+                price: "$75"
             }
-        })
-    }
+        ]
+
+        let getProducts = (data) => {
+            return data;
+        }
+
+        let queryProducts = (timeout, task) => {
+            return new Promise((resolve, reject) => {
+                if (is_stock) {
+                    setTimeout(() => {
+                        resolve(task)
+                    }, timeout);
+                } else {
+                    reject("Error")
+                }
+            })
+        }
+
+        queryProducts(2000, getProducts(data))
+            .then(response => setInformation(response))
+            .catch(err => console.log(err))  
+
+
+    }, []);
+    
 
     return (
         <>
-        {/*
-            ))*/
-            
-            data.map(item => products()
-                             .then(() => 
-                                products(2000, <Item title={item.title} picture={item.picture} price={item.price}> </Item>)))
-            }
+        {            
+            information.map(item => <Item title={item.title} picture={item.picture} price={item.price}> </Item>)
+        }
         </>
     );
     
